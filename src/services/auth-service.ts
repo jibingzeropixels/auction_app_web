59bd43165e6b7bfc3a02d72d91c5466f59ec1c97
Roshin Rajesh <roshinrajesh@Roshins-MacBook-Air.local>
2025-03-19 11:52:19 +0530

    Add login services

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -31,5 +31,29 @@ export const authService = {
       console.error('Registration error:', error);
       throw error;
     }
+  },
+  login: async (
+    email: string,
+    password: string
+  ) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password})
+      });
+      
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Login failed');
+      }
+      
+      return await response.json();
+    } catch (error) {
+      console.error('Login error:', error);
+      throw error;
+    }
   }
 };
